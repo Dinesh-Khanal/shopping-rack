794d58af2af401250e150add24263986a4abbbb9
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2022-11-11 17:14:29 +0545

    Database connection completed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import app from "./app";
 import dotenv from "dotenv";
+import dbConnect from "./config/database";
 
-dotenv.config({ path: "./src/config/config.env" });
+// Handling Uncaught Exception
+process.on("uncaughtException", (err: Error) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({ path: "./src/config/config.env" });
+}
+
+dbConnect();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err: Error) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
