a6c88fbee7bae0a1c453f5f394c612c8068d5f15
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2022-12-14 18:34:03 +0545

    menu added

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div style={{ padding: 12 }}>
+    <section>
       <h1>Nepal shopping rack</h1>
       <h3>Enjoy your online shopping experience</h3>
-    </div>
+    </section>
   );
 };
 
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,41 @@
+/* eslint-disable simple-import-sort/imports */
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-
+import { Outlet, Link } from 'react-router-dom';
+import { FaBars, FaTimes, FaSearch } from 'react-icons/fa';
 import styles from '../styles/App.module.css';
 const Layout = () => {
   return (
     <main className={styles.container}>
-      <header>Nepal Shoping logo | login | signup</header>
+      <input type="checkbox" id={styles.check} />
+      <nav>
+        <div className={styles.logo}>
+          Shop<span>R</span>ack
+        </div>
+        <div className={styles.search_box}>
+          <input type="search" placeholder="Search Products" />
+          <span>
+            <FaSearch />
+          </span>
+        </div>
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/">contacts</Link>
+          </li>
+          <li>
+            <Link to="/">about</Link>
+          </li>
+          <li>
+            <Link to="/">login</Link>
+          </li>
+        </ul>
+        <label htmlFor={styles.check} className={styles.bar}>
+          <FaBars id={styles.bars} />
+          <FaTimes id={styles.times} />
+        </label>
+      </nav>
       <Outlet />
       <footer>This is footer section</footer>
     </main>
