8d09752c84de521eebbe9770083a0c7c9137e45b
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-01-12 10:45:45 +0545

    test for product route added

---
+++ backend/src/__test__/product.test.ts
@@ -0,0 +1,56 @@
+import app from "../app";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
+import mongoose from "mongoose";
+import Product from "../models/productModel";
+//import { createProduct } from "../controllers/productController";
+
+describe("product router", () => {
+  const productPayload = {
+    name: "xyz",
+    description: "sample project",
+    price: 200,
+    category: "sample",
+    Stock: 5,
+  };
+  const userPayload = {
+    email: "example@xyz.com",
+    password: "apple@321",
+  };
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
+  });
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+  });
+  describe("product is empty", () => {
+    it("should return 400", async () => {
+      const productId = "product123";
+      const res = await request(app).get(`/api/product/${productId}`);
+      expect(res.statusCode).toEqual(400);
+    });
+    it("returns status code 401 if user is not signed", async () => {
+      const res = await request(app)
+        .post("/api/admin/product/new")
+        .send(productPayload);
+      expect(res.statusCode).toEqual(401);
+    });
+    it("should return a 200 status and the product", async () => {
+      const product = await Product.create(productPayload);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      const { _body, statusCode } = await request(app).get(
+        `/api/product/${product._id}`
+      );
+      expect(statusCode).toBe(200);
+      expect(_body.product.name).toBe(product.name);
+    });
+    it("returns status code 200 if user is not signed in", async () => {
+      const res = await request(app).post("/api/login").send(userPayload);
+      console.log(res.statusCode);
+      //expect(res.statusCode).toEqual(200);
+    });
+  });
+});
