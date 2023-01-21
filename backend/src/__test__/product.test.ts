adf5ad13a44bc4087a492780254ba38334070c11
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-01-21 11:37:23 +0545

    test case added in product router in backend

--- backend/src/__test__/product.test.ts
+++ backend/src/__test__/product.test.ts
@@ -1,14 +1,20 @@
 import app from "../../app";
 import request from "supertest";
 import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
 describe("product router", () => {
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
+  });
   describe("product is empty", () => {
     it("should return 404", async () => {
       const productId = "product-123";
       const res = await request(app).get(`/api/products/${productId}`);
       expect(res.statusCode).toEqual(404);
     });
-    it("returns status code 201 if product is passed", async () => {
+    it("returns status code 401, unauthorized user can not create product", async () => {
       const sampleProduct = {
         name: "xyz",
         description: "sample project",
@@ -19,10 +25,11 @@ describe("product router", () => {
       const res = await request(app)
         .post("/api/admin/product/new")
         .send(sampleProduct);
-      expect(res.statusCode).toEqual(201);
+      expect(res.statusCode).toEqual(401);
     }, 200000);
   });
   afterAll(async () => {
+    await mongoose.disconnect();
     await mongoose.connection.close();
   });
 });
